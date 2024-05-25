f3df8767483b149ae24045df91f1c1d7ab06e933
welli7ngton <wellingtonasilva45@gmail.com>
2024-05-25 00:59:58 -0300

    agregacao ts

---
+++ src/poo/agregacao.ts
@@ -0,0 +1,38 @@
+export class CarrinhoDeCompras {
+    private readonly produtos: Produto[] = []
+
+    constructor(){}
+
+    inserirProdutos(...produtos: Produto[]): void {
+        for (let produto of produtos){
+            this.produtos.push(produto)
+        }
+    }
+
+    quantidadeDeProdutos(): number{
+        return this.produtos.length
+    }
+
+    valorTotal(): number{
+        return this.produtos.reduce((soma, produto) => soma + produto.preco, 0)
+    }
+}
+
+export class Produto{
+    constructor(public nome: string, public preco: number){}
+}
+
+const p1 = new Produto('p1', 1.777)
+const p2 = new Produto('p2', 13.46)
+const p3 = new Produto('p3', 10320.42564)
+const p4 = new Produto('p4', 10000.324)
+
+const c1 = new CarrinhoDeCompras()
+
+c1.inserirProdutos(p1)
+c1.inserirProdutos(p2)
+c1.inserirProdutos(p3)
+c1.inserirProdutos(p4)
+
+console.log(c1.quantidadeDeProdutos())
+console.log(c1.valorTotal())
