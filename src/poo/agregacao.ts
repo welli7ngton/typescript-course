ed654bd41a52218212050c2707d49811b55a9529
welli7ngton <wellingtonasilva45@gmail.com>
2024-05-25 12:54:39 -0300

    corrigindo estrutura do código(prettier)

--- src/poo/agregacao.ts
+++ src/poo/agregacao.ts
@@ -1,38 +1,41 @@
 export class CarrinhoDeCompras {
-    private readonly produtos: Produto[] = []
+  private readonly produtos: Produto[] = [];
 
-    constructor(){}
+  constructor() {}
 
-    inserirProdutos(...produtos: Produto[]): void {
-        for (let produto of produtos){
-            this.produtos.push(produto)
-        }
+  inserirProdutos(...produtos: Produto[]): void {
+    for (const produto of produtos) {
+      this.produtos.push(produto);
     }
+  }
 
-    quantidadeDeProdutos(): number{
-        return this.produtos.length
-    }
+  quantidadeDeProdutos(): number {
+    return this.produtos.length;
+  }
 
-    valorTotal(): number{
-        return this.produtos.reduce((soma, produto) => soma + produto.preco, 0)
-    }
+  valorTotal(): number {
+    return this.produtos.reduce((soma, produto) => soma + produto.preco, 0);
+  }
 }
 
-export class Produto{
-    constructor(public nome: string, public preco: number){}
+export class Produto {
+  constructor(
+    public nome: string,
+    public preco: number,
+  ) {}
 }
 
-const p1 = new Produto('p1', 1.777)
-const p2 = new Produto('p2', 13.46)
-const p3 = new Produto('p3', 10320.42564)
-const p4 = new Produto('p4', 10000.324)
+const p1 = new Produto('p1', 1.777);
+const p2 = new Produto('p2', 13.46);
+const p3 = new Produto('p3', 10320.42564);
+const p4 = new Produto('p4', 10000.324);
 
-const c1 = new CarrinhoDeCompras()
+const c1 = new CarrinhoDeCompras();
 
-c1.inserirProdutos(p1)
-c1.inserirProdutos(p2)
-c1.inserirProdutos(p3)
-c1.inserirProdutos(p4)
+c1.inserirProdutos(p1);
+c1.inserirProdutos(p2);
+c1.inserirProdutos(p3);
+c1.inserirProdutos(p4);
 
-console.log(c1.quantidadeDeProdutos())
-console.log(c1.valorTotal())
+console.log(c1.quantidadeDeProdutos());
+console.log(c1.valorTotal());
